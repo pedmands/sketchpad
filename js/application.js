013b59ccecd5d1858ab6a5b7621ff487eea5ccd0
Preston Edmands <pedmands@gmail.com>
2015-07-30 13:59:20 -0400

    Finished

--- js/application.js
+++ js/application.js
@@ -1,37 +1,42 @@
 
 $(document).ready(function () {
-	
-	
-	//set grid
+//set initial grid
 	size = 10;
 	var fieldWidth = 700;
-	//draw grid
+//draw initial grid
 	for (var y = 0; y < size; y++) {
 	    for (var x = 0; x < size; x++) {
 	        $('#container').append("<div class='square'></div>");
 	    }
 	      $("#container").append("<div class='row'></div>");
     }
-    //resize cells
+//resize cells
     divSize = Math.round(fieldWidth/size);
     $('.square').width(divSize);
     $('.square').height(divSize);
-    //color cells
-    $('.square').on('mouseenter', function(){
-	    $(this).addClass('colored');
-    });
     
+//cell color-ator
+    $('.square').on('mouseenter', function(){
+	    $(this).addClass('red');
+    });
+
+//reset button
 $('#reset').on('click', function(){
 	var input = prompt("Please enter a number between 1 and 64", "");
+	if(input > 0 && input < 64){
 	$('#container').clear;
 	drawGrid(input);
+	}
+	else{
+	alert("you crazy!")};
 });
 
+//reset draw
 function drawGrid(i){
 	document.getElementById("container").innerHTML = "";
 	size = i;
 	var fieldWidth = 700;
-	//draw grid
+	//draw new grid
 	for (var y = 0; y < size; y++) {
 	    for (var x = 0; x < size; x++) {
 	        $('#container').append("<div class='square'></div>");
@@ -42,10 +47,12 @@ function drawGrid(i){
     divSize = Math.round(fieldWidth/size);
     $('.square').width(divSize);
     $('.square').height(divSize);
+    
     //color cells
-    $('.square').on('mouseenter', function(){
-	    $(this).addClass('colored');
+   $('.square').on('mouseenter', function(){
+	    $(this).addClass('red');
     });
 }
+
 
 });
