e7a006087710b4870722e6b9d494636efae880fc
Preston Edmands <pedmands@gmail.com>
2015-07-30 13:44:44 -0400

    Added reset button

--- js/application.js
+++ js/application.js
@@ -1,21 +1,51 @@
 
 $(document).ready(function () {
+	
+	
 	//set grid
-	var number = 16;
+	size = 10;
+	var fieldWidth = 700;
 	//draw grid
-	for (var y = 0; y < number; y++) {
-	    for (var x = 0; x < number; x++) {
+	for (var y = 0; y < size; y++) {
+	    for (var x = 0; x < size; x++) {
 	        $('#container').append("<div class='square'></div>");
 	    }
-	    
 	      $("#container").append("<div class='row'></div>");
     }
     //resize cells
-    divSize = (600)/number;
+    divSize = Math.round(fieldWidth/size);
     $('.square').width(divSize);
     $('.square').height(divSize);
     //color cells
     $('.square').on('mouseenter', function(){
 	    $(this).addClass('colored');
     });
+    
+$('#reset').on('click', function(){
+	var input = prompt("Please enter a number between 1 and 64", "");
+	$('#container').clear;
+	drawGrid(input);
+});
+
+function drawGrid(i){
+	document.getElementById("container").innerHTML = "";
+	size = i;
+	var fieldWidth = 700;
+	//draw grid
+	for (var y = 0; y < size; y++) {
+	    for (var x = 0; x < size; x++) {
+	        $('#container').append("<div class='square'></div>");
+	    }
+	      $("#container").append("<div class='row'></div>");
+    }
+    //resize cells
+    divSize = Math.round(fieldWidth/size);
+    $('.square').width(divSize);
+    $('.square').height(divSize);
+    //color cells
+    $('.square').on('mouseenter', function(){
+	    $(this).addClass('colored');
+    });
+}
+
 });
