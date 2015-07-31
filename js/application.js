1c8692bca50868dcfe97d951a13cfc04a681fa8e
Preston Edmands <pedmands@gmail.com>
2015-07-30 22:38:42 -0400

    cleaned up application javascript

--- js/application.js
+++ js/application.js
@@ -1,25 +1,8 @@
 
 $(document).ready(function () {
 //set initial grid
-	size = 10;
-	var fieldWidth = 700;
-//draw initial grid
-	for (var y = 0; y < size; y++) {
-	    for (var x = 0; x < size; x++) {
-	        $('#container').append("<div class='square'></div>");
-	    }
-	      $("#container").append("<div class='row'></div>");
-    }
-//resize cells
-    divSize = Math.round(fieldWidth/size);
-    $('.square').width(divSize);
-    $('.square').height(divSize);
-    
-//cell color-ator
-    $('.square').on('mouseenter', function(){
-	    $(this).addClass('red');
-    });
-
+	drawGrid(12);
+	
 //reset button
 $('#reset').on('click', function(){
 	var input = prompt("Please enter a number between 1 and 64", "");
